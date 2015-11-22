4b14e7c529a1c2a545b01969b34b68db671dffac
f1lander <filanderuclez@gmail.com>
2015-11-22 02:17:46 -0600

    sdaS

--- src/MEGAGame.js
+++ src/MEGAGame.js
@@ -2,17 +2,24 @@ var MEGAGame = (function(){
 	
 	function MEGAGame(phaserGame) {
 		this.game = phaserGame;
-	
+		this.Ivis;
+		this.bg_music;
+		this.hasStarted = false;
+
 	}
  
- var Ivis;
- var bg_music;
- //var cursors;
+TTTGame.prototype.startGame = function() {
+		this.hasStarted = true;
+		this.logo.visible = false;
+		this.counter.visible = true;
+		this.tapToStart.visible = false;
+		this.tapToStart.blinker.stopBlinking();
+	};
 
 MEGAGame.prototype.preload = function() {		
 		this.game.load.image('backg', 'assets/FONDO.png');
 		this.game.load.image('backgINV', 'assets/FONDO_INVERTED.png');
-		this.game.load.image('Ivis','assets/rollar_new.png');		
+		this.game.load.image('this.Ivis','assets/rollar_new.png');		
     	this.game.load.audio('fall_velocity', 'assets/Falling_Velocity.ogg');
 };
 
@@ -27,22 +34,22 @@ MEGAGame.prototype.preload = function() {
  MEGAGame.prototype.create = function() {
 	this.game.physics.startSystem(Phaser.Physics.P2JS);
  	//this.game.world.setBounds(0, 0, 600, 3000);
- 	bg_music = this.game.add.audio('fall_velocity');
+ 	this.bg_music = this.game.add.audio('fall_velocity');
  	cursors = this.game.input.keyboard.createCursorKeys();
  	//for(var i = 0; i < 3; i++){
  	this.background1 = this.game.add.sprite(0, 0, 'backg');
  	this.background2 = this.game.add.sprite(0, 600, 'backgINV'); 	
  	  	
-	Ivis = this.game.add.sprite(this.game.world.centerX - 75, 100,'Ivis');
-	Ivis.anchor.setTo(0.5, 0.5);
-	Ivis.scale.setTo(0.05, 0.05);
-	this.game.physics.p2.enable(Ivis);
+	this.Ivis = this.game.add.sprite(this.game.world.centerX - 75, 100,'this.Ivis');
+	this.Ivis.anchor.setTo(0.5, 0.5);
+	this.Ivis.scale.setTo(0.05, 0.05);
+	this.game.physics.p2.enable(this.Ivis);
 
-	this.game.camera.follow(Ivis);
+	this.game.camera.follow(this.Ivis);
 		
-	bg_music.play("", 0, 1, true);
-	bg_music.onLoop.add(playLevelMusic, this);
-	//bg_music.play();
+	this.bg_music.play("", 0, 1, true);
+	this.bg_music.onLoop.add(playLevelMusic, this);
+	//this.bg_music.play();
 };
 
 MEGAGame.prototype.moveBackground = function(background){
@@ -54,60 +61,69 @@ MEGAGame.prototype.moveBackground = function(background){
 };
 
 function playLevelMusic(){
-	bg_music.play("", 0, 1, true);
+	this.bg_music.play("", 0, 1, true);
 }
 
-			 
+
+TTTGame.prototype.startGame = function() {
+	this.hasStarted = true;
+	this.logo.visible = false;
+	this.counter.visible = true;
+	this.tapToStart.visible = false;
+	this.tapToStart.blinker.stopBlinking();
+};
+		 
 MEGAGame.prototype.update = function() {
-		//game.debug.spriteInfo(Ivis, 20, 32);
-		// Ivis.y++;
-	  	//Ivis.body.reverse(150);
+		//game.debug.spriteInfo(this.Ivis, 20, 32);
+		// this.Ivis.y++;
+	  	//this.Ivis.body.reverse(150);
 		
  		this.moveBackground(this.background1);
   		this.moveBackground(this.background2);
 
-	  	console.log("angle: " + Ivis.body.angle)
+	  	console.log("angle: " + this.Ivis.body.angle)
 	  	if (cursors.left.isDown)
 	  	{
-	  		if(Ivis.body.angle<30)
+	  		if(this.Ivis.body.angle<30)
 	  		{
-	    		Ivis.body.rotateRight(50);
+	    		this.Ivis.body.rotateRight(50);
 	  		}else{
-	  			Ivis.body.angle = 30;
+	  			this.Ivis.body.angle = 30;
 	  		}
 	  	}   //ship movement
 	    else if (cursors.right.isDown)
 	    {	
-	    	if(Ivis.body.angle>-30)
+	    	if(this.Ivis.body.angle>-30)
 	    	{
-	  			Ivis.body.rotateLeft(50);
+	  			this.Ivis.body.rotateLeft(50);
 	    	}else{
-	    		Ivis.body.angle = -30;
+	    		this.Ivis.body.angle = -30;
 	    	}
 	    }
 	    else 
 	    {
-	    	Ivis.body.setZeroRotation();
+	    	this.Ivis.body.setZeroRotation();
 	    }
 	    
 	    if (cursors.down.isDown)
 		{
-			Ivis.body.reverse(150);
+			this.Ivis.body.reverse(150);
 		}else if (cursors.up.isDown)
 		{
-			Ivis.body.thrust(150);
+			this.Ivis.body.thrust(150);
 		}
 
-		console.log("X: " +  Ivis.x + " Y: " + Ivis.y);		
+		console.log("X: " +  this.Ivis.x + " Y: " + this.Ivis.y);		
+	};
+
+	MEGAGame.prototype.render = function() {
+
+	    game.debug.cameraInfo(game.camera, 32, 32);
+	    game.debug.spriteCoords(player, 32, 500);
+
 	};
 
 	return MEGAGame;
 
 })();
 
-function render() {
-
-    game.debug.cameraInfo(game.camera, 32, 32);
-    game.debug.spriteCoords(player, 32, 500);
-
-}
